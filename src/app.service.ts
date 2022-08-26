ebcd04aff9bbf58829e033a21067017fecfa428e
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-26 12:51:55 -0300

    Install nest config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return `NODE_ENV: ${process.env.NODE_ENV}`;
+    return `DB_URI: ${this.configService.get<string>("DB_URI")}`;
   }
 }
