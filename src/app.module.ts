850c127803117c4eb4e5d65a4c96bb1e2525c795
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-26 13:35:07 -0300

    Try another string

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
-console.log(`${process.cwd()}/.env.${process.env.NODE_ENV}`);
+console.log(process.env.NODE_ENV);
+console.log(`.env.${process.env.NODE_ENV}`);
 
 @Module({
   imports: [
