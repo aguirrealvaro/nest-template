88a8cbc1d856bd74c5fe541b7c1d7fc439f653d9
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-29 16:06:16 -0300

    Create configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigurationModule } from "./config/config.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
-      isGlobal: true,
-    }),
-  ],
+  imports: [ConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
